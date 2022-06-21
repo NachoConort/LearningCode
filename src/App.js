8cb3cb2108d5448203428c3c079d64324a54b00b
NachoConort <nachoconort@gmail.com>
2022-06-21 11:03:19 -0300

    Deleted innecesary lines in index.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 // Importamos el archivo App.css para darle estilo al contenido del componente App.
 import './App.css';
-
+// Importamos el componente Testimonio.
+import Testimonio from './components/Testimonio';
 // Este es un componente, lo sabemos porque el nombre de la funcion comienza con mayuscula.
 // Conceptualmente, los componentes son como las funciones de JavaScript. 
 function App() {
   return (
+    // Dentro del div principal-container, usamos nuestro componente.
     <div className="App">
+      <div className='principal-container'>
+        <h1>Aprendiendo React</h1>
+        <Testimonio />
+      </div>
     </div>
   );
 }
-
 // Exportamos el componente, para luego importarlo en otro archivo y poder utilizarlo alli.
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,21 +3,16 @@
 // Podemos instalar dependencias que queramos utilizar haciendo npm install en la terminal.
 // Podemos eliminar algunos archivos de Test que son innecesarios.
 // Los archivos index.js e index.html deben existir para que se compile el proyecto.
-
 // Debemos importar React como hacemos abajo para utilizar esta biblioteca.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 // Aqui importamos el index.css que le dara unos estilos a la pagina.
 import './index.css';
-
 // Aqui importamos el componente del archivo App.js.
 import App from './App';
-
 // Para mostrar lo que queramos en nuestra App, utilizaremos el div con id root, en el cual mostraremos todo.
 // Obtenemos este div con document.getElementById.
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 // Ejecutamos root.render, y dentro lo que queremos mostrar.
 root.render(
   <React.StrictMode>
