ad3aa47174b3124771e5fb32f3319e9e65b3e0ed
NachoConort <nachoconort@gmail.com>
2022-06-20 22:50:27 -0300

    Added comments in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+// Para crear una App de React, utilizamos el comando npx create-react-app nombre-app en la terminal.
+// Podemos eliminar algunos archivos de Test que son innecesarios.
+// Los archivos index.js e index.html deben existir para que se compile el proyecto.
+
+// Debemos importar React como hacemos abajo para utilizar esta biblioteca.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Aqui importamos el index.css que le dara unos estilos a la pagina.
 import './index.css';
 import App from './App';
 
+// Para mostrar lo que queramos en nuestra App, utilizaremos el div con id root, en el cual mostraremos todo.
+// Obtenemos este div con document.getElementById.
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Ejecutamos root.render, y dentro lo que queremos mostrar.
 root.render(
   <React.StrictMode>
     <App />
