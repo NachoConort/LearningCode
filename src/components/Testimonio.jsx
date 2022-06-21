e6397953fba923b41043c3bd5de7c10ef4da4718
NachoConort <nachoconort@gmail.com>
2022-06-21 10:58:00 -0300

    Added img and props in Testimonio component

--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -4,12 +4,17 @@
 import React from 'react';
 
 // Creamos nuestro componente
-function Testimonio () {
+// Puedo pasar datos a un componente por medio de algo llamado props.
+function Testimonio (props) {
   // Dentro del return estara lo que queremos mostrar.
   return (
     // Con className asignamos una clase.
     <div className='testimonio-container'> 
-      
+      <img 
+        className='testimonio-img'
+        src={require(`../img/${props.img}.png`)}/>
     </div>
   )
-}
+};
+// Exportamos el componente.
+export default Testimonio;
