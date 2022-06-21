fdbd58792c822c07a43e1291c6cec36502c10f3e
NachoConort <nachoconort@gmail.com>
2022-06-21 19:18:33 -0300

    Added Boton component

---
+++ src/components/Boton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Boton({ texto, esBotonClic, manejarClic }) {
+  return(
+    <button className={esBotonClic ? 'boton-clic' : 'boton-reiniciar'}
+    onClick={manejarClic}>
+      {texto}
+    </button>
+  )
+}
