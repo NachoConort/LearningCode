19ef94fce6b7abcc3424f5e63bfb2a8c219e5a2e
NachoConort <nachoconort@gmail.com>
2022-06-21 19:15:34 -0300

    Added Contador component

--- src/components/SecondSection.jsx
+++ src/components/SecondSection.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import Contador from './Contador';
+import { useState } from 'react';
 
 function SecondSection() {
-
-}
+  const [numClics, setNumClics] = useState(0)
+  const manejarClic = () => {
+    setNumClics(numClics + 1)
+  };
+  const restartContador = () => {
+    setNumClics(0)
+  };
+  return(
+    <div className='second-section'>
+      <div className='contenedor-contador'>
+        <Contador
+        texto= 'Clic' 
+        numClics={numClics}
+        esBotonClic= {true} />
+      </div>
+    </div>
+  )
+};
