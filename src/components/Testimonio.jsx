35b114ea96270eef542b36e3b337a81a57cf6524
NachoConort <nachoconort@gmail.com>
2022-06-21 11:14:46 -0300

    Finished Testimonio component

--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -13,6 +13,12 @@ function Testimonio (props) {
       <img 
         className='testimonio-img'
         src={require(`../img/${props.img}.png`)}/>
+        <div className='text-container'>
+        <p className='name-testimonio'> 
+          <strong>{props.name}</strong> en {props.country} </p>
+        <p className='charge-testimonio'> {props.charge} en <strong>{props.company}</strong> </p>
+        <p className='text-testimonio'> "{props.testimonio}" </p>
+      </div>
     </div>
   )
 };
