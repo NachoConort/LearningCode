5883e626513f1de031e7c3c5bbab9135d608fae3
NachoConort <nachoconort@gmail.com>
2022-06-28 13:01:37 -0300

    Deleted unnecesary lines

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import FirstSection from './FirstSection'
-import SecondSection from './components/SecondSection';
+import FirstSection from './FirstSection';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <FirstSection />
-    <SecondSection />
   </React.StrictMode>
 );
