768a67bb5533b81700e016bd05b790499012865c
NachoConort <nachoconort@gmail.com>
2022-06-28 12:38:24 -0300

    Deleted unnecesary lines

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../stylesheets/Contador.css';
 // Se puede desestructurar el objeto props y pasarle al componente directamente la propiedad que quiero usar.
-// Esto se realiza con llaves{}.
 function Contador({ numClics }) {
   return (
     <div className='contador'>
