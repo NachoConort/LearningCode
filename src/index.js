d509ddf71c807805063d312059860613afcc1131
NachoConort <nachoconort@gmail.com>
2022-06-28 13:42:02 -0300

    Imported BrowserRouter, Routes. Route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import FirstSection from './FirstSection';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
