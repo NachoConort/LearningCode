d3fd537c3886f7f0f41e4dc528fcd767726938a2
NachoConort <nachoconort@gmail.com>
2022-06-28 15:54:42 -0300

    Imported bootstrap css and js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import FirstSection from './FirstSection';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
